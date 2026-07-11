3a978a1bf998d9c94849ef3d6d2edd4f88be6202
Kevin Deng <sxzz@sxzz.moe>
2026-07-11 13:50:01 +0900

    fix: patch tsgo version

--- scripts/tsgo-version.ts
+++ scripts/tsgo-version.ts
@@ -7,6 +7,6 @@ const filePath = path.resolve(
 )
 const fileContent = await readFile(filePath, 'utf8')
 const date = new Date().toISOString().split('T', 1)[0].replaceAll('-', '')
-const newContent = fileContent.replaceAll('7.0.0-dev', `7.0.0-dev.${date}`)
+const newContent = fileContent.replaceAll('-dev', `-dev.${date}`)
 await writeFile(filePath, newContent)
-console.info(`Patched tsgo version to 7.0.0-dev.${date}`)
+console.info(`Patched tsgo version to dev.${date}`)
